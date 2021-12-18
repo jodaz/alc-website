364515b94f57f1b841d69b11bd3feff280e53ea8
Luisandra29 <luisandrasifontes@gmail.com>
2021-12-18 11:42:56 -0400

    agregar iconos en nav

--- resources/views/website/navs/nav.blade.php
+++ resources/views/website/navs/nav.blade.php
@@ -6,14 +6,29 @@
 
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="{{ url('/#inicio') }}" style="color: #FFFFFF;" title="Inicio">inicio</a>
+                <li class="nav-item">                    
+
+                    <a class="nav-link js-scroll-trigger" href="{{ url('/#inicio') }}" style="color: #FFFFFF;" title="Inicio">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" width="26" height="28" viewBox="0 0 24 24" fill="currentColor">
+                          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                        </svg>
+                    inicio</a>
+
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="{{ url('/#actualidad') }}" style="color: #FFFFFF;" title="Actualidad">actualidad</a>
+                    <a class="nav-link js-scroll-trigger" href="{{ url('/#actualidad') }}" style="color: #FFFFFF;" title="Actualidad">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
+                          <path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd" />
+                        </svg>
+                    actualidad</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="{{ route('news') }}" style="color: #FFFFFF;" title="Noticias">Noticias</a>
+                    <a class="nav-link js-scroll-trigger" href="{{ route('news') }}" style="color: #FFFFFF;" title="Noticias">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" width="30" height="30" viewBox="0 0 24 24" fill="currentColor">
+                          <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
+                          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+                        </svg>
+                    Noticias</a>
                 </li>
             </ul>
         </div>
